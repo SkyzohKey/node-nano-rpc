78b209faef5df9b37ca3c2f0b9fa6fc5a1881689
SkyzohKey <skyzohkey@protonmail.com>
2018-01-24 19:07:21 +0100

    Remove dependency on request. Close #2
    This commit does remove the `request` module from the code/dependencies. This saves some
    extra bytes of bundle size. Also, as HTTP logic is handled in ONLY ONE place in the `#_send
    method` this should introduce no issues.
    
    So node-raiblocks-rpc is now DEPENDENCIES FREE !
    
    I also fixed the tests for `#account_info` that were broken because I was getting the `done`
    function from the `it` block instead of `describe` one. Now tests passes in ~60ms!
    
    Enjoy! #XRBrocks

--- examples.js
+++ examples.js
@@ -3,10 +3,20 @@ const RaiClient = require("./lib");
 const RAI_ADDRESS =
   "xrb_3t6k35gi95xu6tergt6p69ck76ogmitsa8mnijtpxm9fkcm736xtoncuohr3";
 
-const client = new RaiClient("http://[::1]:7076");
+const client = new RaiClient("http://127.0.0.1:7076");
+
+const handleErrors = e => {
+  console.log("\nError: " + e.message);
+};
+
 client
   .account_balance(RAI_ADDRESS)
-  .then(account => console.log("Account balance:", account));
-client.block_count().then(count => console.log("Block count:", count));
+  .then(account => console.log("Account balance:", account))
+  .catch(this.handleErrors);
+
+client
+  .block_count()
+  .then(count => console.log("Block count:", count))
+  .catch(this.handleErrors);
 
 //client.stop().then(() => console.log("Node stopped. Exiting..."));
